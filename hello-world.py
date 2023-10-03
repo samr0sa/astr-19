e8c162edddd5a5e4ee27b9cf293e149b635a1c14
samr0sa <146674956+samr0sa@users.noreply.github.com>
2023-10-03 12:54:34 -0700

    Added a hello world proram
    New program file

---
+++ hello-world.py
@@ -0,0 +1,16 @@
+#This program will write Hello World!
+
+
+#This function tells the computer to print "Hello World!"
+
+
+def PrintHelloWorld():
+	print("Hello World!")
+
+#This defines out main() function for our program
+def main():
+	PrintHelloWorld()
+
+#When we run the program, this executes first
+if __name__=="__main__":
+	main()
