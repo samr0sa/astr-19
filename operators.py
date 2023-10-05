1f7f7915c521260df3f2c1a643bbe71e24dbe9ab
samr0sa <saalrosa@ucsc.edu>
2023-10-05 12:53:55 -0700

    Notes from Session 3

---
+++ operators.py
@@ -0,0 +1,26 @@
+x = 9
+y = 3
+
+print('x+y = ', x+y)
+print('x-y = ', x-y)
+print('x*y = ', x*y)
+print('x/y = ', x/y)
+print('x%y = ', x%y)
+print('x**y = ', x**y)
+
+# Assignment
+x = 9
+x += 3
+print('x=9, x+=3; x =',x)
+x = 9
+x -= 3
+print('x=9, x-=3; x =',x)
+x = 9
+x *= 3
+print('x*=3; x =', x)
+print('x; x =',x)
+
+x = 1337
+y = 225
+print("x==y ?", x==y)
+print("x!=y ?", x!=y)
