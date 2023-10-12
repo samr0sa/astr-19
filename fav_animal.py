59703fdc6d051aca025b91ca56806c591016b61c
samr0sa <saalrosa@ucsc.edu>
2023-10-11 21:07:09 -0700

    Session 4 Prompt for Code Journal #1

--- fav_animal.py
+++ fav_animal.py
@@ -1,4 +1,5 @@
-class Axolotl: #create a class to list variables of 
+# create a class to list variables of 
+class Axolotl: 
     def __init__(animal, arm_length, leg_length, eye_num, has_tail, has_fur):
         animal.arm_length = arm_length
         animal.leg_length = leg_length
@@ -6,19 +7,22 @@ class Axolotl: #create a class to list variables of
         animal.has_tail = has_tail
         animal.has_fur = has_fur
 
-    def describe(animal): #describing animal's characteristics
+# describe animal's characteristics given the function below, 
+# along with stating their data type
+    def describe(animal): 
         print()
         print("My favorite animal is an axolotl!")
         print()
-        print("\033[4mAxolotl Description:\033[0m")
-        print(f"- Arm Length: {animal.arm_length} cm")
-        print(f"- Leg Length: {animal.leg_length} cm")
-        print(f"- Number of Eyes: {animal.eye_num}")
-        print(f"- Does it have a tail?: {'Yes' if animal.has_tail else 'No'}")
-        print(f"- Is it furry?: {'Yes' if animal.has_fur else 'No'}")
+        print(f"- Arm Length: {animal.arm_length} cm - (Type: {type(animal.arm_length).__name__})")
+        print(f"- Leg Length: {animal.leg_length} cm - (Type: {type(animal.leg_length).__name__})")
+        print(f"- Number of Eyes: {animal.eye_num} - (Type: {type(animal.eye_num).__name__})")
+        print(f"- Does it have a tail?: {'Yes' if animal.has_tail else 'No'} - (Type: {type(animal.has_tail).__name__})")
+        print(f"- Is it furry?: {'Yes' if animal.has_fur else 'No'} - (Type: {type(animal.has_fur).__name__})")
         print()
 
-my_axolotl = Axolotl(2, 2, 2, True, False)
+# use the __init__ function that has the parameters and 
+# define my_axolotl function along with the respective information
+my_axolotl = Axolotl(2.0, 2.0, 2, True, False) 
 
 my_axolotl.describe()
 
